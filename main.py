6fe053ba82f7f5e09b18f47ade9c79f108cf23c5
sbergcoding <spencerberg24@gmail.com>
2025-01-18 14:01:19 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 def invalid():
     print("Invalid input please try again")
 def prompt_user(choice: int):
+    print("Would you like to create or view a recipie?")
     choice = input("What would you like to do?: ")
 #include filename
 print("Welcome to the recipie book")
 choice = 1
 prompt_user(choice)
+if(choice == 1):
+    print("Creating a recipie")
+elif (choice == 2):
+    print("Viewing recipies")
+else:
+    numb = 1
+    do
+        invalid()
+        
+    
+    while (numb == 1)

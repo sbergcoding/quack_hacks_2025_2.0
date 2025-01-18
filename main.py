a22a53ab285debd454d5be3cbf4bde00b3f3d7bc
Luis D. Hernandez <hernandezmorenoluisdaniel@gmail.com>
2025-01-18 15:42:42 -0800

    Update main.py
    YEsS

--- main.py
+++ main.py
@@ -1,31 +1,20 @@
-
-
-
 def invalid():
-    print("Invalid input please try again")
+    print("Invalid input, please try again")
 
-def prompt_user(choice: int):
-    print("Would you like to create or view a recipie?")
-    choice = input("What would you like to do?: ")
 
-def create():
-    print("Creating a recipie")
-
-def view():
-    print("Viewing recipies")
-
-def ingredients(amount, ingredients[amount, dictionary]):
+def ingredientsList():
     ingredientItem = input("Ingredients: ")
+    ingredientList = ingredientItem.split(", ")
     items = 0
-    for i in range(len(ingredientItem)):
+    print(ingredientList)
+
+    """
+    for i in range(ingredientItem):
+        if i == ",":
+            #delete
+            list.pop(i)
+
+    for i in range(len(list)):
         items += 1
-    list = []
-    list.append(ingredientItem)
-
-
-    for x in dictionary:
-
-        #if list has ingrediants:
-            #return ingrediants 
-#include filename
-
+        """
+ingredientsList()

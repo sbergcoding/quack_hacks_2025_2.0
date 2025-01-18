978ec9364767d18554876cb98b3dba3455a7baa6
sbergcoding <spencerberg24@gmail.com>
2025-01-18 13:45:23 -0800

    updated main.py

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+def invalid():
+    print("Invalid input please try again")
+def prompt_user(choice: int):
+    choice = input("What would you like to do?: ")
+#include filename
 print("Welcome to the recipie book")
-choice = input("Choose your recipie book: ")
+choice = 1
+prompt_user(choice)

445e6ff23dc2ca111397f18330aa9079459e88c9
sbergcoding <spencerberg24@gmail.com>
2025-01-18 14:45:30 -0800

    Update main.py
    modified main.py

--- main.py
+++ main.py
@@ -3,18 +3,13 @@ def invalid():
 def prompt_user(choice: int):
     print("Would you like to create or view a recipie?")
     choice = input("What would you like to do?: ")
-#include filename
-print("Welcome to the recipie book")
-choice = 1
-prompt_user(choice)
-if(choice == 1):
+def create():
     print("Creating a recipie")
-elif (choice == 2):
+def view():
     print("Viewing recipies")
-else:
-    numb = 1
-    do
-        invalid()
-        
-    
-    while (numb == 1)
+def food_items(amount, ingrediants[amount, dictionary]):
+    for x in dictionary:
+
+        #if list has ingrediants:
+            #return ingrediants 
+#include filename

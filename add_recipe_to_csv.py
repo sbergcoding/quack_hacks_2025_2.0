579452a6ad3a313b67bc22f13f3f9626489b407c
noahham <noahham25@gmail.com>
2025-01-19 00:45:49 -0800

    update app

--- add_recipe_to_csv.py
+++ add_recipe_to_csv.py
@@ -17,11 +17,24 @@ def ingredient_values_to_str(ingredients):
 def instructions_to_str(instructions):
     return "\n".join([f"{i+1}. {instruction}" for i, instruction in enumerate(instructions)]) + "\n\n"
 
-def list_to_instructions():
-    pass
+def list_to_instructions(instructions):
+    for i in range(len(instructions)):
+        instructions[i] = str(i+1) + ". " + instructions[i].get()
+    return ", ".join(instructions)
 
+def list_to_ingredients(ingredients):
+    temp = []
+    for item in ingredients:
+        temp.append(item[0].get())
+    return ",".join(temp)
+
+def list_to_amounts(ingredients):
+    temp = []
+    for item in ingredients:
+        temp.append(item[1].get())
+    return ",".join(temp)
 
 # This function takes in the name of the recipe, instructions, ingredients, ingredients_amount, location, servings, time and writes it to the yummy.csv file.
-def input_to_csv(name, instructions, ingredients, ingredients_amount, location, servings, time):
+def input_to_csv(name,  ingredients, ingredients_amount, instructions, servings, time):
     with open("yummy.csv", "a") as f:
-        f.write(f"{name};{instructions};{ingredients};{ingredients_amount};{location};{servings};{time};\n")
+        f.write(f"{name};{ingredients};{ingredients_amount};{instructions};;{servings};{time};\n")

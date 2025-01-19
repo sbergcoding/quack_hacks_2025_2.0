bfc4e9cf365f7fa13347c4a5a75b88e554f12ec6
HappyHamburger <justinmetz991@gmail.com>
2025-01-19 11:21:28 -0800

    Update search.py

--- search.py
+++ search.py
@@ -2,7 +2,7 @@ from recipe import *
 from convert import *
 from add_recipe_to_csv import input_to_csv
 def search(input_ingredients=[""]) -> list["Recipe"]:
-    """Searches using ingredients and a user input. Returns a list of recipes that match the filter"""
+    """Searches using ingredients. Returns a list of recipes that match the filter"""
     filter_list=[]
     for i in range(len(input_ingredients)):
         input_ingredients[i]=input_ingredients[i].lower()
@@ -15,12 +15,9 @@ def search(input_ingredients=[""]) -> list["Recipe"]:
             for j in range(len(input_ingredients)):
                 for h in range(len(keys)):
                     if (input_ingredients[j] in keys[h]):
-                        print(keys[h])
                         filter_list.append(library[i])
-
         if (len(filter_list)>=len(input_ingredients)):
             return filter_list
         else: 
             return None
     return -1
-print(search(["Bread"]))

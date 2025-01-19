542bc3d4c2f801de400bc49023403f0eb6ab9053
noahham <noahham25@gmail.com>
2025-01-18 21:23:29 -0800

    update app

--- app.py
+++ app.py
@@ -25,8 +25,7 @@ def toCreate(event):
 def toHome(event):
     app.show_frame(StartPage)
 
-def submitFilters():
-    print("ADD THIS LATER")
+
 
 
 
@@ -176,12 +175,15 @@ class Page1(tk.Frame):
         country = ttk.OptionMenu(buttonFrame, selected, *options)
         country.pack(side='left', ipadx=10, ipady=5, padx=(0, 20))
 
-        submitButton = ttk.Button(buttonFrame, text="Submit", command=submitFilters)
+        submitButton = ttk.Button(buttonFrame, text="Submit", command=self.submitFilters)
         submitButton.pack(ipadx=5, ipady=5)
 
         buttonFrame.pack()
 
-        self.displayRecipe(Recipe("test", "1. test\n2. test\n3. test", {"ingredient": 'asd'}, "", 1, "", ""))
+        #self.displayRecipe(Recipe("test", "1. test\n2. test\n3. test", {"ingredient": 'asd'}, "", 1, "", ""))
+
+    def submitFilters(self):
+        print("ADD THIS LATER")
 
     def update_scroll_region(self, event):
         self.canvas.configure(scrollregion=self.canvas.bbox("all"))
@@ -205,13 +207,94 @@ class Page1(tk.Frame):
 class Page2(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = ttk.Label(self, text="Create", font=H1)
-        label.pack()
 
-        # Switches to homepage
-        button2 = ttk.Button(self, text="Home", command=lambda: controller.show_frame(StartPage))
-        button2.pack()
+        # Canvas
+        self.canvas = tk.Canvas(self)
+        self.canvas.pack(side="left", fill="both", expand=True)
 
+        # Scrollbar
+        scrollbar = ttk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        scrollbar.pack(side="right", fill="y")
+        self.canvas.configure(yscrollcommand=scrollbar.set)
+
+        # Frame
+        self.contentFrame = ttk.Frame(self.canvas)
+        self.canvas.create_window((0, 0), window=self.contentFrame, anchor="nw")
+
+        # Bind frame position to scrollbar
+        self.contentFrame.bind("<Configure>", self.update_scroll_region)
+
+        titleFrame = tk.Frame(self.contentFrame)
+
+        label = ttk.Label(titleFrame, text="Create", font=H1)
+        label.pack(side='left', padx=(20, 200), pady=(20, 0))
+
+        image = Image.open("icons/home.png").convert("RGBA").resize((30, 30))
+        photo = ImageTk.PhotoImage(image)
+
+        # Create the button with the icon
+        homeImage = ttk.Label(titleFrame, image=photo)
+        homeImage.image = photo
+        homeImage.pack(pady=(22, 0))
+
+        titleFrame.pack(pady=(0, 80))
+
+        homeImage.bind("<Button-1>", toHome)
+
+        ingredientsFrame = tk.Frame(self.contentFrame)
+
+        ttk.Label(ingredientsFrame, text="Ingredients", font=H2).pack(side="left", padx=(10, 80), pady=(0, 30))
+
+        image = Image.open("icons/create.png").convert("RGBA").resize((20, 20))
+        photo = ImageTk.PhotoImage(image)
+
+        # Create the button with the icon
+        addImage = ttk.Label(ingredientsFrame, image=photo)
+        addImage.image = photo
+        addImage.pack(side="left", padx=(100, 0), pady=(0, 30))
+
+        ingredientsFrame.pack()
+
+        self.ingredientInputs = []
+        self.createIngredientsInput()
+        self.createIngredientsInput()
+
+        ttk.Label(self.contentFrame, text="Instructions", font=H2).pack(padx=(10, 200), pady=30)
+
+        self.instructionsInputs = []
+        self.createInstructionsInput()
+        self.createInstructionsInput()
+        self.createInstructionsInput()
+
+
+    def update_scroll_region(self, event):
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
+    def createIngredientsInput(self):
+        entryFrame = ttk.Frame(self.contentFrame)
+
+        bullet = ttk.Label(entryFrame, text="\u2022", font=H3)
+        entry1 = ttk.Entry(entryFrame, width=15)
+        entry2 = ttk.Entry(entryFrame, width=10)
+
+        bullet.pack(side="left", padx=20, pady=(0, 5))
+        entry1.pack(side="left", ipady=4, padx=(0, 25), pady=(0, 10))
+        entry2.pack(side="left", ipady=4, pady=(0, 10))
+
+        entryFrame.pack()
+        self.ingredientInputs.append((entry1, entry2))
+
+    def createInstructionsInput(self):
+        entryFrame = ttk.Frame(self.contentFrame)
+
+        num = ttk.Label(entryFrame, text=(str(len(self.instructionsInputs) + 1) + "."), font=H3)
+        entry = ttk.Entry(entryFrame, width=27)
+
+        num.pack(side="left", padx=20, pady=(0, 5))
+        entry.pack(side="left", ipady=4, pady=(0, 10))
+
+        entryFrame.pack()
+        self.instructionsInputs.append(entry)
 
 app = TkinterApp()
 sv_ttk.set_theme("dark")

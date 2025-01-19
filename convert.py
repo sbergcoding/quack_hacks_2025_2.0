3000629e6f098dcd6d667fcf9c5e39440e1a80f9
Scarlett <scarlettrhinehart@gmail.com>
2025-01-18 21:22:44 -0800

    ksgekilsdgkldgwkjl

--- convert.py
+++ convert.py
@@ -4,14 +4,15 @@ import csv
 def convert_csv(path: str) -> dict:
     rec_dict = {}
 
-    with open(path, newline='') as csv_file:
-        reader = csv.DictReader(csv_file, delimiter=';')
+    with open(path, newline='', encoding='utf8') as csv_file:
+        reader = csv.DictReader(csv_file, delimiter=";")
 
         for row in reader:
+            print(row.keys())
             name = row['name']
-            instruc = row['instructions']
             ingred = row['ingredients']
-            ingred_amount = row['ingredients amount']
+            ingred_amount = row['ingredients_amount']
+            instruc = row['instructions']
             loc = row['location']
             serv = row['servings']
             time = row['time']
